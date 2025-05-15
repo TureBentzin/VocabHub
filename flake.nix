44ef959a247ba7298fc7826ad36694b437ba9aec
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-05-15 07:46:09 +0000

    finish maven build

--- flake.nix
+++ flake.nix
@@ -17,21 +17,26 @@
     in
     {
       packages.x86_64-linux.default = maven.buildMavenPackage rec {
-        pname = "VocabHub";
+        pname = "vocabhub";
         version = "1.0-SNAPSHOT";
 
         src = ./.;
 
-        mvnHash = "";
+        mvnHash = "sha256-2w0HzcCJo0FfFqSQw+wTvQIV6ZbsCrcIHkPtZrQZE3o=";
+        mvnJdk = jdk23;
 
-        nativeBuildInputs = with pkgs; [ makeWrapper ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
 
         installPhase = ''
+          runHook preInstall
+
           mkdir -p $out/bin $out/share/vocabhub
-          install -Dm644 ./target/VocabHub-1.0-SNAPSHOT.jar $out/share/VocabHub-1.0-SNAPSHOT.jar
+          install -Dm644 ./target/VocabHub-1.0-SNAPSHOT.jar $out/share/vocabhub/VocabHub-1.0-SNAPSHOT.jar
 
           makeWrapper ${jdk23}/bin/java $out/bin/vocabhub \
             --add-flags "-jar $out/share/vocabhub/VocabHub-1.0-SNAPSHOT.jar"
+
+          runHook postInstall
         '';
       };
 
